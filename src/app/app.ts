bbb9c10b13b262d7f97a8d32d6835fcd8693f9d2
PorKeat <alexkgm2412@gmail.com>
2025-07-27 13:08:52 +0700

    modified:app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProductService } from './services/product.service';
-import { CartService } from './services/cart.service';
 
 interface Product {
   id: number;
